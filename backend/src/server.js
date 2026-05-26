e1b4531fb704352e22fa018667ad5b72fdccf978
Daksh <dakshkundu30102002@gmail.com>
2026-05-27 01:15:07 +0530

    deployement config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import path from 'path';
 const app = express(); 
+
+const __dirname = path.resolve();
 
 app.get('/health', (req, res) => {
     res.status(200).json({msg:"api is up and running"});
 });
+
+app.get('/books', (req, res) => {
+    res.status(200).json({msg:"Books endpoint"});
+});
+
+if(ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}" , (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
 
 const port = ENV.PORT;
 
